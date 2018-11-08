2b0f8c4b9986b2553148d55e7fa0a8c55e6d064c
Filip Kucharczyk <fku-ext@danskecommodities.com>
2018-11-08 19:05:32 +0100

    add optimized version of bubble sort

--- src/main/java/bubble/Main.java
+++ src/main/java/bubble/Main.java
@@ -7,6 +7,10 @@ public class Main {
         printArray(array);
         bubbleSort(array);
         printArray(array);
+
+        array = new int[]{1,10,100,21,5};
+        bubbleSort_optimized(array);
+        printArray(array);
     }
 
     private static void printArray(int[] array) {
@@ -20,6 +24,16 @@ public class Main {
     public static void bubbleSort(int[] array) {
         for (int i = 0; i < array.length; i++) {
             for (int j = 0; j < array.length - 1; j++) {
+                if(array[j] > array[j+1]) {
+                    swap(j, j+1, array);
+                }
+            }
+        }
+    }
+
+    public static void bubbleSort_optimized(int[] array) {
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array.length - 1 - i; j++) {
                 if(array[j] > array[j+1]) {
                     swap(j, j+1, array);
                 }
